fc5e3c6be95f80c46328347b6f29570450f93564
Victor Tran <victor.n.tran@outlook.com>
2018-04-17 20:08:23 -0400

    no big substantial changes to tests

--- src/_test_/extractword.js
+++
@@ -1,9 +0,0 @@
-const extractWord = (sentence = '') => {
-    if (!sentence) return [''];
-    console.log(sentence.split(' '));
-    return sentence.split(' ');
-}
-
-module.exports = {
-    extractWord: extractWord
-}
---
+++ src/_test_/validateAddress.test.js
@@ -0,0 +1,5 @@
+import { validateAddress } from "../functionLib/validateAddress";
+
+test('something', () => {
+    expect(true).toBe(true);
+})
