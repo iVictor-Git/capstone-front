f40c0402877cdb4f76a2f07c1267f1fbd343ee90
Victor Tran <victor.n.tran@outlook.com>
2018-04-19 18:35:11 -0400

    Created similar component to basic profile component

---
+++ src/components/ContactProfileInformation/ContactProfileInformation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProfileField from '../ProfileField/ProfileField';
+
+const ContactProfileInformation = ({ className, phone, socials }) => {
+    return (
+        <div className={className}>
+            {/* Field Component, value=x and name=x */}
+            <ProfileField value={phone} name={'Number'} />
+            <ProfileField value={socials} name={'Social'} />
+        </div>
+    )
+}
+
+export default ContactProfileInformation;
