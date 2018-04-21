426b47918526b61ef67814ea5b14449001a0d37b
Victor Tran <victor.n.tran@outlook.com>
2018-04-21 19:18:58 -0400

    Fetch resource from onboard api, stored into apartment finder state as places

--- src/components/ApartmentFinder/ApartmentFinder.js
+++ src/components/ApartmentFinder/ApartmentFinder.js
@@ -7,24 +7,46 @@ class ApartmentFinder extends Component {
         super(props);
         this.state = {
             search: '',
+            places: [],
 
         }
     }
 
     handleChange = (event) => {
-        this.setState({search: event.target.value});
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        fetch(`https://search.onboard-apis.com/propertyapi/v1.0.0/property/address?postalcode=${this.state.search}&propertytype=APARTMENT&page=1&pagesize=20`, {
+            method: 'get',
+            headers: {
+                'Accept': 'application/json',
+                'apikey': '216a286caf9ec8435f1b49e09a31207c'
+            }
+        })
+            .then(response => response.json())
+            .then(response => this.setState({places: response.property}, 
+                () => console.log(this.state.places)))
+            .catch(err => console.log('Something went wrong fetching properties'));
+        console.log('submitted');
     }
 
     render() {
-        const {api_keys} = this.props;
+        // const {api_keys} = this.props;
         return (
-            <div className='apartment-finder-component-container'>
+            <div className='apartment-finder-component-container container'>
                 <div className='apartment-finder-title-container'>
                     <h1>Apartment search</h1>
                 </div>
                 <div className='apartment-finder-search-container'>
-                    <form action="" onSubmit={(event) => event.preventDefault()}>
-                        <AutoComplete api_keys={api_keys}/>
+                    <form action="" onSubmit={this.handleSubmit}>
+                        <div className='form-group'>
+                            <input className='form-control' onChange={this.handleChange} type="text" name='search' placeholder='Enter a zip code' />
+
+                        </div>
+                        {/* <AutoComplete api_keys={api_keys}/> */}
                     </form>
                 </div>
 
