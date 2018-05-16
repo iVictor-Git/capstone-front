6cb97a019da77b554450c1f181532746fc5b95b8
Victor Tran <victor.n.tran@outlook.com>
2018-05-16 16:30:35 -0400

    Functionality to Google Maps component -> Pan to lat/lng of selected and multiple markers

--- src/components/UI/Body/Body.js
+++ src/components/UI/Body/Body.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import classes from './Body.css';
-// import Home from './Home/Home';
+import Home from './Home/Home';
 // import Register from '../../../containers/Forms/Register/Register';
 // import Login from '../../../containers/Forms/Login/Login';
 // import Chat from '../../Chat/Chat';
 import ApartmentFinder from '../../../containers/ApartmentFinder/ApartmentFinder';
 // import Profile from '../../Profile/Profile';
-// import { defaultUserProfile } from '../../const/const';
+// import { defaultUserProfile } from '../../../const/const';
 
 
 class Body extends Component {
@@ -27,16 +27,15 @@ class Body extends Component {
 
     render() {
         const { handleSignIn } = this;
-        const { api_keys } = this.props;
         return (
             <div className={classes.Body}>
                 {/* <Home /> */}
                 {/* <Register onSignIn={handleSignIn} /> */}
                 {/* <Login onSignIn={handleSignIn} /> */}
                 {/* <Chat user={this.props.user} /> */}
-                <ApartmentFinder api_keys={api_keys} />
-                {/* <Profile 
-                user={defaultUserProfile}
+                <ApartmentFinder />
+                {/* <Profile
+                    user={defaultUserProfile}
                 // user={this.props.user}
                 /> */}
             </div >
